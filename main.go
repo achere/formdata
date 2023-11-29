d8e4247ebcf7712b3362638deaef7826af164a42
Alexander Chereshnev <achere@Alexanders-MacBook-Pro.local>
2023-11-29 19:41:15 +0100

    Read body twice

--- main.go
+++ main.go
@@ -1,34 +1,39 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/form", func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.Body)
+		fmt.Println("START REQUEST")
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
 			http.Error(w, "beb", http.StatusBadRequest)
 		}
-		fmt.Println(body)
 		bodyString := string(body)
 		fmt.Println(bodyString)
+		r.Body.Close()
+		r.Body = io.NopCloser(bytes.NewReader(body))
 		fmt.Println("BODY STRING FINISH, KEY VALUE BEGIN")
-		r.ParseMultipartForm(10000)
-		for key, value := range r.Form {
-			log.Println(key, value)
+		r.ParseMultipartForm(10 << 20)
+		for key, values := range r.PostForm {
+			for _, value := range values {
+				fmt.Printf("%s=%s\n", key, value)
+			}
 		}
+
 		fmt.Println("KEY VALUE FINISH, HEADERS BEGIN")
 		for key, values := range r.Header {
 			for _, value := range values {
 				fmt.Printf("%s: %s\n", key, value)
 			}
 		}
+		fmt.Println("END REQUEST")
 	})
 
 	http.ListenAndServe(":8080", mux)
