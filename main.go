5a07e06cdc2dd4844d996cd8cc860df0454d741f
Alexander Chereshnev <achere@Alexanders-MacBook-Pro.local>
2023-11-30 10:55:43 +0100

    Add forwarding

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 )
 
 func main() {
@@ -35,7 +36,39 @@ func main() {
 			}
 		}
 		fmt.Println("END REQUEST")
+		r.Body.Close()
+		r.Body = io.NopCloser(bytes.NewReader(body))
+		target, err := url.Parse("https://emea2.owndata.com/api/v1/services/1104801/gdpr/forget")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		req := &http.Request{
+			Method:        r.Method,
+			URL:           target,
+			Proto:         r.Proto,
+			ProtoMajor:    r.ProtoMajor,
+			ProtoMinor:    r.ProtoMinor,
+			Header:        r.Header,
+			Body:          r.Body,
+			ContentLength: r.ContentLength,
+			Host:          target.Host,
+		}
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+		for key, values := range resp.Header {
+			for _, value := range values {
+				w.Header().Add(key, value)
+			}
+		}
+		w.WriteHeader(resp.StatusCode)
+		io.Copy(w, resp.Body)
 	})
-
 	http.ListenAndServe(":8080", mux)
 }
